0e3348f82485075a2b7e65e1007a7f97b77895a5
Abbasqulu Rzayev <arzayevv10@gmail.com>
2025-09-26 23:55:04 +0400

    Update Question2.sql

--- kaggle-postgres-netflix/sql/Question2.sql
+++ kaggle-postgres-netflix/sql/Question2.sql
@@ -1,4 +1,6 @@
 -- 2. Find all shows that have more than 2 seasons and an IMDb score higher than 8.0.
 SELECT title,seasons,imdb_score
 FROM titles 
-WHERE seasons > 2 AND imdb_score > 8.0;
+
+WHERE seasons > 2 AND imdb_score > 8.0
+ORDER BY title;
